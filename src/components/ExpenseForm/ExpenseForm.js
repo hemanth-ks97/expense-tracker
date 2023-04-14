d691f451fb4c690357b5f3504f74643bd23a5ae8
Hemanth Kalyana Sundaram <hks@Hemanths-MacBook-Pro.local>
2023-04-14 07:55:16 -0500

    Resetting the form input fields onSubmit

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -8,33 +8,46 @@ const ExpenseForm = () => {
     
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
-        console.log(event.target.value);
+        //console.log(event.target.value);
     }
 
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
-        console.log(event.target.value);
+        //console.log(event.target.value);
     }
 
     const dateChangeHandler = (event) => {
         setenteredDate(event.target.value);
-        console.log(event.target.value);
+        //console.log(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const formData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: enteredDate
+        }
+        console.log(formData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setenteredDate('');
     }
 
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expese__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text' onBlur={titleChangeHandler}/>
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01" onBlur={amountChangeHandler}/>
+                    <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type='date' min="2020-01-01" max="2023-12-31" onBlur={dateChangeHandler}/>
+                    <input type='date' min="2020-01-01" max="2023-12-31" value={enteredDate} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__action">
