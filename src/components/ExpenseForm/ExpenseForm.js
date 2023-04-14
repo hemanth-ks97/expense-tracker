26afda452ac1c3083b843b0c4ce0763a2a740191
Hemanth Kalyana Sundaram <hks@Hemanths-MacBook-Pro.local>
2023-04-14 07:31:55 -0500

    Added event handlers to the form

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,20 +1,40 @@
+import { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setenteredDate] = useState('');
+    
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+        console.log(event.target.value);
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        console.log(event.target.value);
+    }
+
+    const dateChangeHandler = (event) => {
+        setenteredDate(event.target.value);
+        console.log(event.target.value);
+    }
+
     return(
         <form>
             <div className="new-expese__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text'/>
+                    <input type='text' onBlur={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01"/>
+                    <input type='number' min="0.01" step="0.01" onBlur={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type='date' min="2020-01-01" max="2023-12-31"/>
+                    <input type='date' min="2020-01-01" max="2023-12-31" onBlur={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__action">
