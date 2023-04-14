5bf735e0e6945e5051e5be7a561dfeeb7e252d56
Hemanth Kalyana Sundaram <hks@Hemanths-MacBook-Pro.local>
2023-04-13 21:16:21 -0500

    First Commit - UI base

---
+++ src/components/UI/Card.js
@@ -0,0 +1,12 @@
+import './Card.css'
+
+function Card(props) {
+    const style = "card "+props.className
+    return(
+        <div className={style}>
+            {props.children}
+        </div>
+    );
+}
+
+export default Card;
